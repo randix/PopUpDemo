30df40d688276dacef280268d428589949245a53
Randall Dow <randall.dow@fs.utc.com>
2020-11-01 08:39:13 -0800

    Update PopUpDemoApp.swift

--- PopUpDemoApp.swift
+++ PopUpDemoApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct PopUpDemoApp: App {
+    @Environment(\.scenePhase) private var phase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            case .active:
+                // App became active
+                print("active")
+            case .inactive:
+                // App became inactive
+                print("inactive")
+            case .background:
+                // App is running in the background
+                print("background")
+            @unknown default:
+                // Fallback for future cases
+                print("default")
+            }
         }
     }
 }
